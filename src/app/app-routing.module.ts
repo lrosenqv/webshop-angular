66fed5601399491aa25dc46f9a545a56595a6346
lrosenqv <kimle.rosenqvist@gmail.com>
2022-03-06 00:03:22 +0100

    structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductsComponent } from './components/products/products.component';
-import { ShoppingcartComponent } from './components/shoppingcart/shoppingcart.component';
-import { StartPageComponent } from './components/start-page/start-page.component';
+import { ProductsComponent } from './pages/shopping/products/products.component';
+import { ShoppingcartComponent } from './pages/shopping-cart/shoppingcart/shoppingcart.component';
+import { StartPageComponent } from './pages/home/start-page/start-page.component';
+import { AdminComponent } from './pages/admin/admin.component';
 
 const routes: Routes = [ 
   { path: 'products', component: ProductsComponent },
   { path: 'shopping-cart', component: ShoppingcartComponent },
   { path: 'start-page', component: StartPageComponent },
+  { path: 'admin', component: AdminComponent },
   { path: '', redirectTo: '/start-page', pathMatch: 'full' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { ProductsComponent } from './components/products/products.component';
+import { ProductsComponent } from './pages/shopping/products/products.component';
 import { MainmenuComponent } from './components/mainmenu/mainmenu.component';
-import { ProductfilterComponent } from './components/productfilter/productfilter.component';
-import { ShoppingcartComponent } from './components/shoppingcart/shoppingcart.component';
+import { ProductfilterComponent } from './pages/shopping/productfilter/productfilter.component';
+import { ShoppingcartComponent } from './pages/shopping-cart/shoppingcart/shoppingcart.component';
 import { ProductHoverDirective } from './directives/product-hover.directive';
-import { StartPageComponent } from './components/start-page/start-page.component';
-import { UserFormComponent } from './components/user-form/user-form.component';
+import { StartPageComponent } from './pages/home/start-page/start-page.component';
+import { UserFormComponent } from './pages/shopping-cart/user-form/user-form.component';
+import { ShoppingCartComponent } from './pages/shopping-cart/shopping-cart.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { LoginComponent } from './components/login/login.component'; 
 
 @NgModule({
-  declarations: [AppComponent, ProductsComponent, MainmenuComponent, ProductfilterComponent, ShoppingcartComponent, ProductHoverDirective, StartPageComponent, UserFormComponent],
+  declarations: [AppComponent, ProductsComponent, MainmenuComponent, ProductfilterComponent, ShoppingcartComponent, ProductHoverDirective, StartPageComponent, UserFormComponent, ShoppingCartComponent, AdminComponent, LoginComponent],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
