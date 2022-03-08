ffdd0b2b3520e5aa19b44efd9b8529585b11b5d9
lrosenqv <kimle.rosenqvist@gmail.com>
2022-03-08 01:03:32 +0100

    login functionality

--- src/app/components/login/services/login.service.ts
+++ src/app/components/login/services/login.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { userLS } from 'src/app/models/userToLS';
 import { IUser } from '../models/IUser';
 
 @Injectable({
@@ -7,6 +9,13 @@ import { IUser } from '../models/IUser';
 })
 export class LoginService {
 private existingUsers: IUser[] = [];
+personToLS: userLS = {username: '', isAdmin: false};
+
+private isOnline = new Subject<userLS[]>();
+isOnline$ = this.isOnline.asObservable();
+
+private online = new Subject<boolean>();
+online$ = this.online.asObservable();
 
   constructor(private http: HttpClient) { }
 
@@ -23,16 +32,22 @@ private existingUsers: IUser[] = [];
     let passwordExist = this.existingUsers.find(exist => exist.password == user.password)
 
     if(personExist && passwordExist){
-      console.log("yay");
+      this.personToLS = {username: personExist.username, isAdmin: personExist.isAdmin}
 
       if(personExist.isAdmin === true){
-        console.log("is also admin")
-        return true;
+        localStorage.setItem('onlineUser', JSON.stringify(this.personToLS))
       }
-      return console.log("not admin");
+      localStorage.setItem('onlineUser', JSON.stringify(this.personToLS))
+      this.online.next(true)
+    } else {
+      this.online.next(false)
     }
-    else if(!personExist || !passwordExist) {
-      console.log("nay");
+  }
+
+  checkOnline(){
+    let userInLS: userLS[] = JSON.parse(localStorage.getItem('onlineUser') || '[]')
+    if(userInLS){
+      this.isOnline.next(userInLS)
     }
   }
 }
